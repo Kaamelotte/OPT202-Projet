22fa3aade21b029784c08efc1ebd816394882c5d
Kaamelotte <48158127+Kaamelotte@users.noreply.github.com>
2021-02-22 21:19:19 +0100

    nan mais vraiment ça prend trop de place dans ma tête pour ce que c'est !

---
+++ option.m
@@ -0,0 +1,17 @@
+function options = option(maxit = 20, quad = 1, verb = 1, rl = 1, tol = [1.e-8,1.e-8,1.e-8] )
+	options.tol(1) = tol(1); # sur le gred du laplacien
+	options.tol(2) = tol(2); # sur les conditions d egalite
+	options.tol(3) = tol(3); # sur le min des multi de lagrange - les conditions d inegalite
+	
+	options.maxit = maxit;
+
+	options.quad = quad;# Utilisation solveur quadratique ou non, obligatoire pour TP4
+		#0 sans solveur quadratique
+		# 1 avec solveur quadratique
+		# 2  avec et sans solveur quadratique
+
+	options.rl = rl; #Recherche lineaire
+	options.verb = verb
+	
+	return
+end;
