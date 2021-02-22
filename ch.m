f5b8cfb7beb282bb026ebf0756f247231c7541d5
Kaamelotte <48158127+Kaamelotte@users.noreply.github.com>
2021-02-22 15:08:28 +0100

    Switch optimsateur et simulateur parce que c'est plus pratique

--- ch.m
+++ ch.m
@@ -11,61 +11,21 @@ global A B L R S color
 ##============================================================================##
 
 castest = '2a'
-[L,xy,A,B,R,S] = casTest(castest);
 color = 'r';
 test = '0'
-
-##================ Simulateur ================================================##
-switch test
-	case '1' ##=== Trace de la chaine ===##	  
-		disp('trace de la chaine');
-		hold on 
-		chs(1,xy,[], []);
-		chs(6,xy,[], []);
-		hold off
-	case '2'  ##=== Energie potentielle et contraintes ======##
-		disp("calcul de l'energie et des contraintes");
-		[e,ce,ci,~,~,~,~,indic] = chs(2,xy,[],[])
-	case '4'  ##=== Gradient de e et jacobienne de c  =================##
-		disp("calcul du gradient de e et de la jacobienne de c");
-		[~,~,~,g,ae,ai,~,indic] = chs(4,xy,[],[])
-	case '5' ##=== Hessien du lagrangien ============##
-		disp("calcul du hessien du lagrangien");
-		[~,~,~,~,~,~,hl,indic] = chs(5,xy,[],[])
-		full(hl)
-	case 'grad' ##=== Verification du gradient de l'energie potentielle ===##
-		disp("verification du gradient de l'energie potentielle"); 
-		verifierGradient(xy);
-	case 'cholmod' ##=== Test de la fonction cholmod ===##
-		disp("Test de la fonction cholmod\n"); 
-		#Tolerance
-		tol_small = 1.e-5; 
-		tol_big = 1.e+5;
-		[~,~,~,~,~,~,hl,indic] = chs(5,xy,[],[]);
-		[L, d, flag] = cholmod(hl, tol_small, tol_big);
-		full(hl)
-		full(L)
-		D = diag(d)
-		M = L*D*L'
-		disp("M est bien defini positive\n")
-		eig(M) #valeur propre de M
-		disp("Erreur d'approximation de hl\n")
-		E =  sum(sum(abs(hl - M)) )
-end##============================================================================##
 
 ##================ Optimiseur ================================================##
 options.tol(1) = 1.e-8; # sur le gred du laplacien
 options.tol(2) = 1.e-8; # sur les conditions d egalite
 options.tol(3) = 1.e-8; # sur le min des multi de lagrange - les conditions d inegalite
 options.maxit = 10;
-
-options.quad = 2# Utilisation solveur quadratique ou non, obligatoire pour TP4
-	#0 sans
-	# 1 avec
-	# 2 les deux
+options.quad = 2
+	#0 sans solveur quadratique
+	# 1 avec solveur quadratique
+	# 2 avec et sans solveur quadratique
 
 options.rl = 1; #Recherche lineaire
-options.verb = 0;
+options.verb = 0; #Choix affichage
 
 if test == '0'
 	
@@ -75,6 +35,7 @@ if test == '0'
 	options.quad = 2
 	[x, lme, lmi, ~] = res(castest, options);
 
+end
 	
 	
 	
@@ -110,8 +71,7 @@ if test == '0'
 	
 	
 	
-	
-	if options.verb == 3
+	if options.verb == 3 &&  test == '0'
 		##=== calcul de la hessienne reduite =======================================##
 		disp("Calcul de la hessienne reduite")
 		[~,ce,ci,g,ae,ai,~,indic] = chs(4,x,lm,[]);
@@ -148,3 +108,44 @@ if test == '0'
 	end;
 end
 ##============================================================================##
+
+
+
+##================ Simulateur ================================================##
+[L,xy,A,B,R,S] = casTest(castest);
+switch test
+	case '1' ##=== Trace de la chaine ===##	  
+		disp('trace de la chaine');
+		hold on 
+		chs(1,xy,[], []);
+		chs(6,xy,[], []);
+		hold off
+	case '2'  ##=== Energie potentielle et contraintes ======##
+		disp("calcul de l'energie et des contraintes");
+		[e,ce,ci,~,~,~,~,indic] = chs(2,xy,[],[])
+	case '4'  ##=== Gradient de e et jacobienne de c  =================##
+		disp("calcul du gradient de e et de la jacobienne de c");
+		[~,~,~,g,ae,ai,~,indic] = chs(4,xy,[],[])
+	case '5' ##=== Hessien du lagrangien ============##
+		disp("calcul du hessien du lagrangien");
+		[~,~,~,~,~,~,hl,indic] = chs(5,xy,[],[])
+		full(hl)
+	case 'grad' ##=== Verification du gradient de l'energie potentielle ===##
+		disp("verification du gradient de l'energie potentielle"); 
+		verifierGradient(xy);
+	case 'cholmod' ##=== Test de la fonction cholmod ===##
+		disp("Test de la fonction cholmod\n"); 
+		#Tolerance
+		tol_small = 1.e-5; 
+		tol_big = 1.e+5;
+		[~,~,~,~,~,~,hl,indic] = chs(5,xy,[],[]);
+		[L, d, flag] = cholmod(hl, tol_small, tol_big);
+		full(hl)
+		full(L)
+		D = diag(d)
+		M = L*D*L'
+		disp("M est bien defini positive\n")
+		eig(M) #valeur propre de M
+		disp("Erreur d'approximation de hl\n")
+		E =  sum(sum(abs(hl - M)) )
+end##============================================================================##
