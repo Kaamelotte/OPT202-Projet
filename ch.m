bfdeccd41f99cb0aaad05c70668dfe118a84ef17
Kaamelotte <48158127+Kaamelotte@users.noreply.github.com>
2021-02-22 23:20:00 +0100

    je te permet pas de juger notre main il est tout propre !

--- ch.m
+++ ch.m
@@ -8,8 +8,8 @@ color = 'r';  lgd = "resultat";
 ## TP4: '4a' '4b' '4c'                                                    ##
 ##============================================================================##
 
-castest = '4b'
-test = 'c'
+castest = '4c'
+test = 'g'
 
 ##================ Optimiseur ================================================##
 options = option();
@@ -28,53 +28,9 @@ if test == '0'
 					info.tol(1),info.tol(2), info.tol(3));
 		fprintf('\n--------------------------------------\n');
 	end	
+else##================ Simulateur ================================================##
+	checker(castest, test = 1);
 end
-
-##================ Simulateur ================================================##
-[L,xy,A,B,R,S] = casTest(castest);
-xy
-switch test
-	case '1' ##=== Trace de la chaine ===##	  
-		disp('trace de la chaine');
-		hold on;
-		chs(1,xy,[], []);
-		chs(6,xy,[], []);
-		hold off;
-	case 'c'  ##=== Energie potentielle et contraintes ======##
-		disp("calcul de l'energie et des contraintes");
-		[e,ce,ci,~,~,~,~,indic] = chs(2,xy,[],[])
-		length(xy)
-		length(R)
-		length(ce)
-		length(ci)
-	case 'g'  ##=== Gradient de e et jacobienne de c  =================##
-		disp("calcul du gradient de e et de la jacobienne de c");
-		[~,~,~,g,ae,ai,~,indic] = chs(4,xy,[],[])
-	case 'hl' ##=== Hessien du lagrangien ============##
-		disp("calcul du hessien du lagrangien");
-		[~,~,~,~,~,~,hl,indic] = chs(5,xy,[],[]);
-		hl = full(hl)
-		valuerPropre = eig(hl)
-		sum(eig(hl)>= 0)/length(eig(hl)) 
-	case 'grad' ##=== Verification du gradient de l'energie potentielle ===##
-		disp("verification du gradient de l'energie potentielle"); 
-		verifierGradient(xy);
-	case 'cholmeod' ##=== Test de la fonction cholmeod ===##
-		disp("Test de la fonction cholmeod\n"); 
-		#Tolerance
-		tol_small = 1.e-5; 
-		tol_big = 1.e+5;
-		[~,~,~,~,~,~,hl,indic] = chs(5,xy,[],[]);
-		[L, d, flag] = cholmeod(hl, tol_small, tol_big);
-		full(hl)
-		full(L)
-		D = diag(d)
-		M = L*D*L'
-		disp("M est bien defini positive\n")
-		eig(M) #valeur propre de M
-		disp("Erreur d'approximation de hl\n")
-		E =  norm(hl - M,inf)
-end##============================================================================##
 
 if options.verb == 3 &&  test == '0'
 	##=== calcul de la hessienne reduite =======================================##
@@ -112,6 +68,3 @@ if options.verb == 3 &&  test == '0'
   fprintf('\n')
 end;
 ##============================================================================##
-
-
-
---
+++ checker.m
@@ -0,0 +1,48 @@
+function [] = checker(castest, test = 1)
+	[L,xy,A,B,R,S] = casTest(castest);
+	global A B L R S color lgd;
+	xy
+	switch test
+		case '1' ##=== Trace de la chaine ===##	  
+			disp('trace de la chaine');
+			hold on;
+			chs(1,xy,[], []);
+			chs(6,xy,[], []);
+			hold off;
+		case 'c'  ##=== Energie potentielle et contraintes ======##
+			disp("calcul de l'energie et des contraintes");
+			[e,ce,ci,~,~,~,~,indic] = chs(2,xy,[],[])
+		case 'g'  ##=== Gradient de e et jacobienne de c  =================##
+			disp("calcul du gradient de e et de la jacobienne de c");
+			[~,~,~,g,ae,ai,~,indic] = chs(4,xy,[],[]);
+			g
+			ae = full(ae)
+			ai = full(ai)
+		case 'hl' ##=== Hessien du lagrangien ============##
+			disp("calcul du hessien du lagrangien");
+			[~,~,~,~,~,~,hl,indic] = chs(5,xy,[],[]);
+			hl = full(hl)
+			valuerPropre = eig(hl)
+			sum(eig(hl)>= 0)/length(eig(hl)) 
+		case 'grad' ##=== Verification du gradient de l'energie potentielle ===##
+			disp("verification du gradient de l'energie potentielle"); 
+			verifierGradient(xy);
+		case 'cholmeod' ##=== Test de la fonction cholmeod ===##
+			disp("Test de la fonction cholmeod\n"); 
+			#Tolerance
+			tol_small = 1.e-5; 
+			tol_big = 1.e+5;
+			[~,~,~,~,~,~,hl,indic] = chs(5,xy,[],[]);
+			[L, d, flag] = cholmeod(hl, tol_small, tol_big);
+			full(hl)
+			full(L)
+			D = diag(d)
+			M = L*D*L'
+			disp("M est bien defini positive\n")
+			eig(M) #valeur propre de M
+			disp("Erreur d'approximation de hl\n")
+			E =  norm(hl - M,inf)
+	end##============================================================================##
+	
+	return
+end
