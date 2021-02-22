4d3049112b2cd00fefb094438c0460a257da76b2
Kaamelotte <48158127+Kaamelotte@users.noreply.github.com>
2021-02-22 21:47:19 +0100

    j'ai  pas 2s a perdre a chaque F5

--- option.m
+++ option.m
@@ -1,4 +1,4 @@
-function options = option(maxit = 20, quad = 1, verb = 1, rl = 1, tol = [1.e-8,1.e-8,1.e-8], save = 1 )
+function options = option(maxit = 10, quad = 1, verb = 1, rl = 1, tol = [1.e-8,1.e-8,1.e-8], save = 1 )
 	options.tol(1) = tol(1); # sur le gred du laplacien
 	options.tol(2) = tol(2); # sur les conditions d egalite
 	options.tol(3) = tol(3); # sur le min des multi de lagrange - les conditions d inegalite
