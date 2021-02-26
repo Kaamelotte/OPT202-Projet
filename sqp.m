4a1aa1f55cdc5e7daa5d7a40b2e1ea4766130559
Kaamelotte <48158127+Kaamelotte@users.noreply.github.com>
2021-02-26 09:14:07 +0100

    fusion sqp/osp : n°2 rajout fct qp / nouveau calcule de F et parametre

--- sqp.m
+++ sqp.m
@@ -28,20 +28,22 @@ function [x,lme, lmi, info] = sqp(simul,x, lme, lmi, options)
 	##=== Verification que l'utilisateur de demande pas plus d'info qu'il n'en veut=========##
 	if options.rl == 1 && options.verb == 2
 		options.verb = 1;
-	end;##====================================================================##
+	end; ##===================================================================##
 	
-	##=== Calcul des multiplicateurs lagrangiens d'egalite=============================##
-    if length(lme) == 0
-		[~,~,~,g,ae,~,~,indic] = simul(4,x,lme);
-		lme = -ae'\g;
-    end
-	m = length(lme);
+	##=== Calcul des multiplicateurs lagrangiens====================================##
+	if length(lme) == 0 
+		    [~,~,~,g,ae,ai,~,indic] = chs(4,x,lme, lmi);
+		    lme = -ae'\g;
+	end
+	if length(lmi) == 0
+		[~,~,~,g,ae,ai,~,indic] = chs(4,x,lme, lmi);
+		lmi = -ai'\g;
+	end
+	me = length(lme);
+	mi = length(lmi);
 	##=======================================================================##
 
-							#####################################
-							##=== Algorithme de Newton ==========##
-							#####################################
-	nbIter = 0;
+	info.niter = 0;
 	nbSimul = 0;
 	alpha = 1;  #pas initial = pas unite
 	normFk = 1;
@@ -56,30 +58,43 @@ function [x,lme, lmi, info] = sqp(simul,x, lme, lmi, options)
 
 ##=== Boucle principale =======================================================##
 	while true
-		[~,ce,~,g,ae,~,~,indic] = simul(4,x,lme);
-
+		[~,ce,ci,g,ae,ai,~,indic] = simul(4,x,lme, lmi);		
 		nbSimul += 1;
-		grdl = g + ae' * lme;
 		
-		##===Quotient des normes de Fk ============================================##
+		grdl = g + [ae ; ai ]' * [lme ; lmi ]
+			
+		##===Quotient des normes de Fk ===========================================##
 		normFkp = max(norm(ce,Inf),norm(grdl,Inf));
 		Q = normFkp / normFk^2;
 		normFk = normFkp;
 		
-		##=== Calcul de la direction de descente ===================================##
-		[~,~,~,~,~,~,hl,~] = simul(5,x,lme);
+		##=== Calcul de la fonction F et de sa derive==================================##
+		[~,~,~,~,~,~,hl,~] = simul(5,x,lme, lmi);
 		nbSimul += 1;
-		dF = [ hl, ae'; ae, zeros(m,m) ];
-		F = [ grdl ; ce ];
+				
+		dF = [ hl, [ae;ai]' ; [ae; ai], zeros(me+mi,me+mi) ];
+		F = [ grdl ; [ce; ci] ];
+		##=====================================================================##
 		
-		dir = -dF\F; #(dk,muk)
-		##===================================================================##
-		
+##=== Calcul de la direction de descente =========================================##
+		if options.quad == 0 				##=== Algorithme de Newton ==========##
+			dir = -dF\F; #(dk,muk)
+			
+		elseif options.quad == 1		##=== Algorithme de Quasi-Newton =====##
+			 #[d, obj, information, lm] = qp (X0             , H, Q,  A,    B, LB, UB, A_LB, A_IN, A_UB)
+			 [dir, obj, information, lm] = qp (ones(n,1) , M,  g, ae, -ce, [] ,  [] ,     []  ,    ai  ,   -ci   );
+			if information.info != 0
+				info.status = information.info;
+				info.niter = info.niter;
+				break;
+			end
+			dir = [dir; lm ];
+		end
+##===Fin calcul de la direction de descente========================================##		
+			
 		#Faut lui trouver une petit place a ce petit chou
 		phi = 0.5 * F' * F; #0.5*||F(z)||^2
 		
-		##===================================================================##
-			
 		if options.verb > 0 ##=== Impression ===##
 			if options.verb == 2			
 				fprintf('---------------------------------------------------------------------------------\n');
@@ -87,7 +102,7 @@ function [x,lme, lmi, info] = sqp(simul,x, lme, lmi, options)
 						'iter','|gl|','|ce|','|x|','|lme|','alpha','phi','Q');
 			end;
 		  fprintf('%4d %10.4e %10.4e %10.4e %10.4e %10.4e %10.4e %10.4e\n',...
-				  nbIter, norm(grdl,inf),norm(ce,inf),norm(x,inf),norm(lme,inf),alpha,phi,Q);
+				  info.niter, norm(grdl,inf),norm(ce,inf),norm(x,inf),norm(lme,inf),alpha,phi,Q);
 		end ##================##    
 			
 							#####################################
@@ -97,16 +112,16 @@ function [x,lme, lmi, info] = sqp(simul,x, lme, lmi, options)
 ##=== Recherche lineaire pour le pas alpha ======================================##
 		if options.rl == 0
 			dphi = F' * dF;     #F(z)^T*F'(z)
-			[alpha, nbSimul] = rl( x, lme, [], dir, simul, nbSimul, dphi, phi, options);
+			[alpha, nbSimul] = rl( x, lme, lmi, dir, simul, nbSimul, dphi, phi, options);
 		end 
 ##=== Fin recherche lineaire ===================================================##
 			
 		##=== Calcul des nouveaux parametres pour Newton ==========================##
 		x = x + alpha*dir(1:n);
-		lme = lme + alpha*dir(n+1:length(dir));
+		lme = lme + alpha*dir(n+1:n+me);
+		lmi = lmi + alpha*dir(n+me+1:length(dir));
 			
-		nbIter = nbIter + 1;
-		info.niter = nbIter;
+		info.niter = info.niter + 1;
 		##=====================================================================##
 
 							#####################################
@@ -116,12 +131,11 @@ function [x,lme, lmi, info] = sqp(simul,x, lme, lmi, options)
 		##=== Test d'optimalite ===================================================##
 		if (norm(grdl,inf) < options.tol(1)) && (norm(ce,inf) < options.tol(2))
 			info.status = 0; #Solution trouvee
-			info.niter = nbIter;
 			break; #Sortie de Newton
 		end##==================================================================##
 			
 		##===Test du nombre d'iterations deja effectuees==============================##
-		if(nbIter >= options.maxit)
+		if(info.niter >= options.maxit)
 			info.status = 2; #Sortie de l'algo car pas de convergence
 			info.tol(1) = norm(grdl,inf);
 			info.tol(2) = norm(ce,inf);
