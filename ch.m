ddd8d92c2f9b03cfb9a40f8776a9487acfc010dc
JustineDeSousa <justine.desousa6@gmail.com>
2021-02-26 12:01:52 +0100

    prise en compte lmi

--- ch.m
+++ ch.m
@@ -9,24 +9,24 @@ color = 'r';  lgd = "resultat";
 ## TP5:        '5a' '5b' '5c'
 ##=========================================================================##
 
-castest = '2b';
+castest = '4a';
 test = '0';
 
 ##================ Optimiseur ===============================================##
 options = option(100);
-options.quad = 0;
-options.verb = 1;
-options.save = 0;
+options.quad = 1;
+options.verb = 2;
+options.save = 1;
 options.rl = 0;
 
 
 if test == '0'	
-	[x, lme, lmei, info] = res(castest, options, color_res_1 = [0.6,0.3,0.6]);
-	info.status
+	[x, lme, lmi, info] = res(castest, options, color_res_1 = [0.6,0.3,0.6]);
+	info.status = info.status
 	if info.status == 2
-        fprintf('--------Condition non remplis--------\n');
-		fprintf('%8s %10s %10s \n',...
-					'|gl|','|ce|','|lmei-ci|');
+        fprintf('--------Condition non remplie--------\n');
+		fprintf('%8s %10s %13s \n',...
+					'|gl|','|ce|','|lmi-ci|');
 		fprintf('%4e %10.4e %10.4e \n',...
 					options.tol(1),options.tol(2), options.tol(3));
 		fprintf('%4e %10.4e %10.4e \n',...
@@ -42,33 +42,35 @@ if options.verb == 3 &&  test == '0'
 	[~,ce,ci,g,ae,ai,~,indic] = chs(4,x,lme,[]);
 	[~,~,~,~,~,~,hl,indic] = chs(5,x,lme,[]);
 
-	N = null(ae)
-	eig(N'*hl*N)
+	N = null([ae;ai])
+	vap = eig(N'*hl*N)
 
 	##=== Conditions suffisantes d'ordre 2 ===##
 	fprintf('---------------------------------\n');
 	fprintf('Verification des CS2:\n');
 	fprintf('---------------------------------\n');
-	fprintf('Grdl(x,lme) =\n');
-	fprintf('%+25.5e\n',g+ae'*lme);
+	fprintf('Grdl(x,lm) =\n');
+	fprintf('%+25.5e\n',g + [ae ; ai ]' * [lme ; lmi ]);
 	fprintf('---------------------------------\n');
-	fprintf('c(x) =\n');
+	fprintf('ce(x) =\n');
 	fprintf('%+25.5e\n',ce);
+	fprintf('---------------------------------\n');
+    fprintf('ci(x) =\n');
+	fprintf('%+25.5e\n',ci);
 	fprintf('---------------------------------\n');
 	fprintf("hl defini positif sur\n le noyau de c'(x)?\n");
 	fprintf('---------------------------------\n');
-	%fprintf("Noyau de c'(x):\n");
-	N = null(ae)
-	%fprintf('---------------------------------\n');
+	N = null([ae;ai]);
 	test = 0;
 	for i=1:size(N,2)
-	    fprintf("|   <Grd^2l(x,lme).z|z> = %f\n",dot(hl*N(:,i),N(:,i)));
+	    fprintf("|   <Grd^2l(x,lm).z|z> = %f\n",dot(hl*N(:,i),N(:,i)));
 	    test += (dot(hl*N(:,i),N(:,i))<=0);
 	end  
 	fprintf('----------------------------------\n');
 	
-	fprintf('%.5f,\n%.5f,',x(1:length(x)/2),x(length(x)/2+1:length(x)))
-
+	fprintf('%.5f,',x(1:length(x)/2));
+    fprintf("\n");
+    fprintf('%.5f,',x(length(x)/2+1:length(x)))
   fprintf('\n')
 end;
 ##=========================================================================##
