94be966f6ba4b95a325c08a8630ae1acb5e30266
JustineDeSousa <justine.desousa6@gmail.com>
2021-03-06 08:35:52 +0100

    enlever la condition sur grdl
    sinon ça converge jamais c'est chiant

--- sqp.m
+++ sqp.m
@@ -27,17 +27,19 @@ function [x,lme, lmi, info] = sqp(simul,x, lme, lmi, options)
 
 	##=== Calcul des multiplicateurs lagrangiens ====================================##
 	[~,ce,ci,g,ae,ai,~,indic] = chs(4,x,lme, lmi);
-    if length(lme) == 0 || (length(ai) > 0 && length(lmi) == 0)
+    if length(ai) > 0 && length(lmi) == 0
         me = size(ae)(1);       mi = size(ai)(1);         m = size([ae; ai])(1);
         lm0 = zeros(m,1);       H = [ae;ai]*[ae;ai]';   q = [ae;ai]*g;
         A = [zeros(1,me), ci']; b = 0;
         lb = zeros(m,1);          ub = repmat(Inf,m,1);
         A_lb = ci;                    A_in = zeros(mi,m);   A_ub = repmat(Inf,mi,1);
-	    #[lm, obj, information, l] = qp (lm0, H, q, A, b, lb, ub, A_lb, A_in, A_ub)
-        lm = -[ae; ai]' \ g
+	    [lm, obj, information, l] = qp (lm0, H, q, A, b, lb, ub, A_lb, A_in, A_ub)
+        #lm = -[ae; ai]' \ g
         lme =  lm(1:me)
         lmi = lm(me+1:m)
-	end
+    elseif length(lme) == 0
+	    lme = -ae'\g;
+    end
 	me = length(lme);
 	mi = length(lmi);
 	##=======================================================================##
@@ -45,60 +47,54 @@ function [x,lme, lmi, info] = sqp(simul,x, lme, lmi, options)
 	info.niter = 0;
 	info.nbSimul = 0;
 	alpha = 1;  #pas initial = pas unite
-	normFk = 1;
 	xm = x;
   
 	if (options.verb == 1) ##=== Impression ===##
-		fprintf('---------------------------------------------------------------------------------\n');
-		fprintf('%4s %7s %10s %10s %10s %10s %10s %11s %10s\n',...
-				'iter','|gl|','|ce|','|ci|','|x|','|lme|','|lmi|','alpha','|dirct|');
-		fprintf('---------------------------------------------------------------------------------\n');
+		fprintf('%4s %7s %10s %10s %10s %10s %10s %11s %10s',...
+				'iter','|gl|','|ce|','|ci|','|x|','|lme|','|lmi|'); fprintf('\n');
 	end ##==============================##  
 
 ##=== Boucle principale =======================================================##
 	while true
-		#=== Mise a jour de donnees ===============================================##
 		[e,ce,ci,g,ae,ai,~,indic] = simul(4,x,lme, lmi);		
 		[~,~,~,~,~,~,hl,~] = simul(5,x,lme, lmi);
 		info.nbSimul += 2;
-
-		grdl = g + [ae ; ai ]' * [lme ; lmi ]
+		grdl = g + [ae ; ai ]' * [lme ; lmi ];
+		#grdl = g + ae'*lme 
 		
-		##=== Test d'optimalite ===================================================##
-		if (norm(grdl,inf) < options.tol(1)) && (norm(ce,inf) < options.tol(2))  && (norm(min(lmi,-ci),inf) < options.tol(3) )
+        ##=== Test d'optimalite ====================================================##
+		if  (norm(ce,inf) < options.tol(2))  && (norm(min(lmi,-ci),inf) < options.tol(3) ) #&&(norm(grdl,inf) < options.tol(1)) 
 			info.status = 0; #Solution trouvee
 			break; #Sortie de la boucle principale
 		end##==================================================================##
 		
-		
-		##=== Quotient des normes de Fk ===========================================##
-		normFkp = max(norm(ce,Inf),norm(grdl,Inf));
-		Q = normFkp / normFk^2;
-		normFk = normFkp;
-		
-		##=== Calcul de la fonction F et de sa derive==================================##
+		##=== Calcul de la fonction F et de sa derive ===================================##
 		if options.quad == 0 || options.rl == 0 ;
 			dF = [ hl, [ae;ai]' ; [ae; ai], zeros(me+mi,me+mi) ];
 			F = [ grdl ; [ce; ci] ];
 			FPQ = [ g ; [ce ; ci ] ];
 		end ##=================================================================##
 		
-##=== Calcul de la dirctection de descente =========================================##
-		if options.quad == 0 			##=== Algorithme de Newton ==========##
-			dirct = -dF\FPQ; #(dk,lmpq)
-		elseif options.quad == 1		##=== Algorithme de Josephy-Newton ===##
+        ##=== Calcul de la direction de descente ======================================##
+		if options.quad == 0          ##=== Algorithme de Newton ==========##
+			dirct = -dF\FPQ; #(dk,lm_PQ)
+		elseif options.quad == 1    ##=== Algorithme de Josephy-Newton ===##
 			if options.deriv == 2
 				[L, d, flag] = cholmod(hl, 1.e-5, 1.e+5);
 				M = L*diag(d)*L';
 			elseif (info.niter == 0) &&(options.deriv == 1)
-				M = eye(n);# matrice initiale
+				M = eye(n); # matrice initiale
 			end
-			#[d, obj, information, lm] = qp (X0             , H, Q,  A,    B, LB, UB, A_LB, A_IN, A_UB)
-			[dk, obj, information, lm] = qp (ones(n,1) , M,  g, ae, -ce, [] ,  [] ,     []  ,    ai  ,   -ci   );
+            M
+            g
+            ae = full(ae)
+            ce
+            ai = full(ai)
+            ci
+			#[d, obj, information, lm] = qp (X0            , H, Q,  A,    B, LB, UB, A_LB, A_IN, A_UB)
+			[dk, obj, information, lm] = qp (zeros(n,1), M,  g, ae, -ce, [] ,  [] ,     []  ,    ai  ,   -ci   );
 			if information.info != 0
-				info.status = information.info;
-                information
-				information.solveiter
+				info.status = information.info
 				break; #on sort de la boucle while principale
 			end
 			dirct = [dk; lm ];
@@ -106,66 +102,58 @@ function [x,lme, lmi, info] = sqp(simul,x, lme, lmi, options)
         dk = dirct(1:n);
 		lmePQ = dirct(n+1:n+me);
 		lmiPQ = dirct(n+me+1:length(dirct));
-        ##=====================================================================##
-        figure(2)
-        hold off;
-        chs(1,x,lme,lmi);
-        hold on;
-        chs(6,x,lme,lmi);
-        hold on;
-        quiver(x(1:n/2),x(n/2+1:n),dk(1:n/2),dk(n/2+1:n));
-        pause()
-        ##=====================================================================##
-##=== Fin calcul de la dirctection de descente =======================================##		
-			
+        ##=== Fin calcul de la dirctection de descente =================================##
+        
+        if (options.verb = 4) && (mod(info.niter,5) == 0) ##=== Plot ===##
+            figure(2)
+            chs(6,x,lme,lmi);
+            chs(1,x,lme,lmi);
+            hold on;
+            quiver(x(1:n/2),x(n/2+1:n),dk(1:n/2),dk(n/2+1:n));
+            title(["itération ", num2str(info.niter)]);
+            pause();
+            hold off;
+        end
+        
 		if options.verb > 0 ##=== Impression ===##
 			if options.verb == 2			
 				fprintf('-------------------------------------------------------------------------------------------\n');
-				fprintf('%4s %7s %10s %10s %10s %10s %10s %11s %10s\n',...
-						'iter','|gl|','|ce|','|ci|','|x|','|lme|','|lmi|','alpha','|dirct|');
+				fprintf('%4s %7s %10s %10s %10s %10s %10s\n',...
+						'iter','|gl|','|ce|','|ci|','|x|','|lme|','|lmi|');
 			end;
-		  fprintf('%4d %10.4e %10.4e %10.4e %10.4e %10.4e %10.4e %10.4e %10.4e\n',...
-				  info.niter, norm(grdl,inf),norm(ce,inf),norm(ci,inf),norm(x,inf),norm(lme,inf),norm(lmi,inf), alpha,norm(dirct,inf));
-		end ##============================##    
+		    fprintf('%4d %10.4e %10.4e %10.4e %10.4e %10.4e %10.4e\n',...
+			    info.niter, norm(grdl,inf),norm(ce,inf),norm(ci,inf),norm(x,inf),norm(lme,inf),norm(lmi,inf));
+		end
 			
-##=== Recherche lineaire pour le pas alpha ========================================##
-		if options.rl == 0
+        
+		if options.rl == 0 ##=== Recherche lineaire pour le pas alpha ===##
 			phi = 0.5 * F' * F; #0.5*||F(z)||^2
 			dphi = F' * dF;     #F(z)^T*F'(z)
 			[alpha, info] = rl( x, lme, lmi, dirct, simul, info, dphi, phi, options);
+        elseif info.niter > 50
+            alpha = 0.5;
 		end 
-##=== Fin recherche lineaire ====================================================##
 		
-		##=== Calcul des nouveaux parametres pour Newton ============================##
-        if info.niter > 50
-            alpha = 0.5
-        end
+		##=== Nouveaux parametres pour Newton ===================================##
 		xm = x;
-		x = x + alpha*dk;
+        x = x + alpha*dk;
 		lme = lme +  alpha*(lmePQ - lme);
 		lmi = lmi +  alpha*(lmiPQ -lmi);
 		
-		if (options.quad == 1) && (options.deriv == 1)
+		if (options.quad == 1) && (options.deriv == 1) ### === BFGS =====================##
 			[M, info] = bfgs(simul, info, M, xm, lme, lmi, dirct, grdl, options );
-		end
-		
+		end 
+        
+		##=== Test du nombre d'iterations deja effectuees =============================##
 		info.niter = info.niter + 1;
-		##=====================================================================##
-
-		##=== Test du nombre d'iterations deja effectuees ==============================##
-		if(info.niter >= options.maxit)
+        if(info.niter >= options.maxit)
 			info.status = 2; #Sortie de l'algo car pas de convergence
 			info.tol(1) = norm(grdl,inf);
 			info.tol(2) = norm(ce,inf);
             info.tol(3) = norm(min(lmi,-ci),inf);
 			break; 
 		end ##=================================================================##
-	end
-##=== Fin Boucle principale ====================================================##
- 
-	if options.verb > 0 ##=== Impression ===##
-		fprintf('-------------------------------------------------------------------------------------------\n');
-	end ##============================## 
-    #close();
+	end ##=== Fin Boucle principale ==============================================##
+    #close(2);
 	return
 end  #Fin de la fonction
